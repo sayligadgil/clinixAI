cadd06de028a23a55d7ee7a6b30a45c889d73945
AniquaGhayas <aniquaghayas@gmail.com>
2026-03-30 21:35:23 +0530

    Organised the files into folders

--- lib/frontend/Patient Interface/clinix_splash_screen.dart
+++ lib/frontend/Patient Interface/clinix_splash_screen.dart
@@ -200,3 +200,39 @@ class CliniXSplashScreen extends StatelessWidget {
     );
   }
 }
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'user_selection_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const UserSelectionScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("ClinixAI Splash Screen"),
+      ),
+    );
+  }
+}
